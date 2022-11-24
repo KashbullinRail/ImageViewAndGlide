f5a9a68dd8cbd7c5810832ad428c75aede8896e5
KashbullinRail <kashbullinrail@gmail.com>
2022-11-24 15:50:27 +0300

    добавил checkBox для поиска без ввода ключевого слова

--- app/src/main/java/com/example/imageviewandglide/MainActivity.kt
+++ app/src/main/java/com/example/imageviewandglide/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.imageviewandglide
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -13,7 +14,10 @@ import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
+
+    private var enable:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,17 +33,22 @@ class MainActivity : AppCompatActivity() {
             }
             return@setOnEditorActionListener false
         }
+        binding.checkBox.setOnClickListener {
+            enableTV()
+        }
+
+
     }
 
     fun getOnRandomImage(): Boolean {
         val keyword = binding.etWordForSearch.text.toString()
-        if (keyword.isBlank()) {
+        if (binding.checkBox.isChecked && keyword.isBlank()) {
             binding.etWordForSearch.error = "Not input text"
             return true
         }
         val inputWord = URLEncoder.encode(keyword, StandardCharsets.UTF_8.name())
         Glide.with(this)
-            .load("https://source.unsplash.com/random?$inputWord")
+            .load("https://source.unsplash.com/random${if(binding.checkBox.isChecked) "?$inputWord" else ""}")
             .skipMemoryCache(true)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
             .placeholder(R.drawable.ic_launcher_foreground)
@@ -47,4 +56,13 @@ class MainActivity : AppCompatActivity() {
 
         return false
     }
+
+    fun enableTV() = with(binding){
+        if (checkBox.isChecked){
+            etWordForSearch.visibility = View.VISIBLE
+        } else {
+            etWordForSearch.visibility = View.GONE
+        }
+    }
+
 }
