19ac06d291845096a94e45c75e5ec1cf20fd8df1
KashbullinRail <kashbullinrail@gmail.com>
2022-11-24 16:00:12 +0300

    добавил ProgressBar

--- app/src/main/java/com/example/imageviewandglide/MainActivity.kt
+++ app/src/main/java/com/example/imageviewandglide/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.imageviewandglide
 
 import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.imageviewandglide.databinding.ActivityMainBinding
 import java.net.URI
 import java.net.URL
@@ -17,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    private var enable:Boolean = false
+    private var enable: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +33,7 @@ class MainActivity : AppCompatActivity() {
             getOnRandomImage()
         }
         binding.etWordForSearch.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH){
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 return@setOnEditorActionListener getOnRandomImage()
             }
             return@setOnEditorActionListener false
@@ -47,18 +52,40 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         val inputWord = URLEncoder.encode(keyword, StandardCharsets.UTF_8.name())
+        binding.progressBar.visibility = View.VISIBLE
         Glide.with(this)
-            .load("https://source.unsplash.com/random${if(binding.checkBox.isChecked) "?$inputWord" else ""}")
+            .load(
+                "https://source.unsplash.com/random" +
+                        "${if (binding.checkBox.isChecked) "?$inputWord" else ""}"
+            )
             .skipMemoryCache(true)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
-            .placeholder(R.drawable.ic_launcher_foreground)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?, model: Any?, target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                   binding.progressBar.visibility = View.GONE
+                    return false
+                }
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+            })
             .into(binding.ivImage)
 
         return false
     }
 
-    fun enableTV() = with(binding){
-        if (checkBox.isChecked){
+    fun enableTV() = with(binding) {
+        if (checkBox.isChecked) {
             etWordForSearch.visibility = View.VISIBLE
         } else {
             etWordForSearch.visibility = View.GONE
